a367bfefdd4f8690d5c5f42e00599871c489a12e
Pushy <support@pushy.me>
2020-12-23 10:52:32 -0500

    Workaround for NullPointerException (FlutterApplicationInfo.flutterAssetsDir) on Flutter 1.22.x+

---
+++ example/android/app/src/main/java/me/pushy/example/Application.java
@@ -0,0 +1,10 @@
+package me.pushy.example;
+
+import io.flutter.app.FlutterApplication;
+
+public class Application extends FlutterApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
